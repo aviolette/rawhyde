0062f07f4bc06cd70ddd6a1c1a8097cda26ac919
aviolette <aviolette@gmail.com>
2014-01-25 10:32:47 -0600

    Cache results from calling services

--- foodtruck.rb
+++ foodtruck.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'tmpdir'
 
 # A tag that creates a href to the chicago food truck finder from a jekyll blog
 # For example,
@@ -14,13 +15,35 @@ module Jekyll
     def initialize(tag_name, foodtruck, tokens)
       super
       @foodtruck_id = foodtruck.gsub(/\s+/, "");
+      @datafile = Dir.tmpdir()+ "/foodtruck_data.txt"
     end
 
     def render(context)
-      uri = URI.parse("http://www.chicagofoodtruckfinder.com/services/trucks/#{@foodtruck_id}")
-      response = Net::HTTP.get_response(uri)
-      foodtruck = JSON.parse(response.body)
-      "<a href='http://www.chicagofoodtruckfinder.com/trucks/#{@foodtruck_id}'>#{foodtruck['name']}</a>"
+      foodtruck_name = read_cached_data
+      if foodtruck_name.nil?
+        uri = URI.parse("http://www.chicagofoodtruckfinder.com/services/trucks/#{@foodtruck_id}")
+        puts "Requesting #{uri.inspect}"
+        response = Net::HTTP.get_response(uri)
+        foodtruck = JSON.parse(response.body)
+        foodtruck_name = foodtruck['name']      
+        write_cached_data(foodtruck_name)
+      end
+      "<a href='http://www.chicagofoodtruckfinder.com/trucks/#{@foodtruck_id}'>#{foodtruck_name}</a>"
+    end
+
+    private
+
+    def write_cached_data(name)
+      File.open(@datafile, 'a') { |f| f.write("#{@foodtruck_id},#{name}\n")}
+    end
+
+    def read_cached_data
+      return nil unless File.exists?(@datafile)
+      File.open(@datafile).each_line do |line|
+        key,value = line.split(',')
+        return value.rstrip if key == @foodtruck_id
+      end
+      return nil
     end
   end
 end
